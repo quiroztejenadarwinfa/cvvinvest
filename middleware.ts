563b74458eca22600d889a537e595d6eefd292db
Financial Platform <soportecvvinvest@proton.me>
2026-01-16 15:07:22 -0500

    Fix: Eliminar middleware.ts deprecated - Resuelve advertencia de Next.js

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// Middleware eliminado - usando localStorage para el idioma
-// This file must exist but can be empty since we're using client-side language switching
-
-export function middleware() {
-  // Middleware disabled - using localStorage approach
-}
-
